28df28b259db9ec2d53b23fa17412bd4d2b7786f
aborn <loveaborn@foxmail.com>
2014-07-20 19:12:01 +0800

    Note:!!setInitial.m may cause the error in call lmnn2

--- setInitial.m
+++ setInitial.m
@@ -2,13 +2,13 @@
 % set the environment for running fro each machine
 
 % addpath(genpath('~/research/lib/lib.matlab'));  % yoks server
-addpath(genpath('/home/aborn/research/lib/lib.matlab')); % aborn-pc
-addpath('/home/aborn/research/code/mlearn/mLMNN2.4/mexfunctions');
-addpath('./rerank');
-addpath('./lib');
-addpath('./train');
+addpath(genpath('/home/aborn/research/lib/lib.matlab')); 
+% addpath('/home/aborn/research/code/mlearn/mLMNN2.4/mexfunctions');
+%addpath('./rerank');
+%addpath('./lib');
+%addpath('./train');
 
-rootpath='/home/aborn/research/code/mlearn';
-mLMNNPath = '/home/aborn/research/code/mlearn/mLMNN2.4';
+%rootpath='/home/aborn/research/code/mlearn';
+%mLMNNPath = '/home/aborn/research/code/mlearn/mLMNN2.4';
 
-addpath(genpath(mLMNNPath));
+%addpath(genpath(mLMNNPath));
